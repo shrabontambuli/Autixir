fe1fbda9c905df09b1a2718f1ad3596f15f3cd27
Shrabon Tambuli <shrabonsa@gmail.com>
2025-02-25 17:59:13 +0600

    create service line in home page

--- src/components/BannerCarousel.jsx
+++ src/components/BannerCarousel.jsx
@@ -41,7 +41,7 @@ const BannerCarousel = () => {
         <div className="relative w-full max-w-7xl mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 {/* Text Section */}
-                <div className="flex flex-col justify-center text-center md:text-left p-4">
+                <div className="flex flex-col justify-center text-center md:text-left p-4 h-96">
                     <h3 className="text-xl text-[#e53e29] font-bold mb-6">{banners[currentIndex].sub_title}</h3>
                     <h1 className="text-2xl lg:text-5xl font-bold mb-6">{banners[currentIndex].title}</h1>
                     <p className="text-lg text-gray-700 mb-10 w-full md:w-10/12">{banners[currentIndex].description}</p>
